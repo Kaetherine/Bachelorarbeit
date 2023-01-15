8300d23b9928b97db000ec471727c313f57d5a8b
katherine <kuhle.katherine@googlemail.com>
2023-01-15 11:41:37 +0100

    added django-plotly-project to repo

--- proxy/proxy_scraper.py
+++ proxy/proxy_scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import time
-from datetime import datetime
+# import time
+# from datetime import datetime
 
 def proxy_scraper():
     url = 'https://free-proxy-list.net/'
