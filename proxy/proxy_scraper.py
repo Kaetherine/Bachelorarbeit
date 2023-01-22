48b5578be01fa555076282789e1a21f1a8672f29
katherine <kuhle.katherine@googlemail.com>
2023-01-22 20:13:31 +0100

    deleted files and projects that are not needed anymore, inserted test_scraper() into file fr proxy_scraper()

--- proxy/proxy_scraper.py
+++ proxy/proxy_scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 # import time
-# from datetime import datetime
+from datetime import datetime
 
 def proxy_scraper():
     url = 'https://free-proxy-list.net/'
@@ -16,26 +16,44 @@ def proxy_scraper():
         'table',attrs={'class': 'table-striped'}
         ).find_all('tr')[1:]:
         tds = row.find_all('td')
-        try:
-            ip = tds[0].text.strip()
-            port = tds[1].text.strip()
-            anonymity = tds[4].text.strip()
-            google = tds[5].text.strip()
-            https = tds[6].text.strip()
-            # code = tds[2].text.strip()
-            # country = tds[3].text.strip()
-            # retreived_on = datetime.date
-            if (
-              https == 'no' and anonymity == 'elite proxy' and google =='yes'
-              ):
-                proxy = str(ip) + ':' + str(port)
-                proxies.append(proxy)
-
-        except Exception as e:
-            # augment exception handling here
+        ip = tds[0].text.strip()
+        port = tds[1].text.strip()
+        anonymity = tds[4].text.strip()
+        google = tds[5].text.strip()
+        https = tds[6].text.strip()
+        # country_code = tds[2].text.strip()
+        # country = tds[3].text.strip()
+        # retreived_on = datetime.date
+        if (
+            https == 'yes' and anonymity == 'elite proxy' and google =='yes'
+            ):
+            proxy = str(ip) + ':' + str(port)
+            proxies.append(proxy)
+        else:
             continue
     return proxies
 
+def test_scraper():
+
+    url = "https://ipinfo.io/json"
+    proxies = proxy_scraper()
+
+    if proxies == []:
+        print('No Proxies.')
+    else:
+        for proxy in proxies:
+            proxies = {
+                'http': proxy,
+                'https': proxy,
+            }
+            try:
+                response = requests.get(url, proxies=proxies)
+                print(response.json())
+            except:
+                print(f'Proxie {proxy} is occupied.')
+                continue
+
+test_scraper()
 
 
 
