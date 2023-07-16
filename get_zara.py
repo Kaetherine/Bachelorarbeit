92e2618625548f469cd8cd0072da16afddc9be10
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-16 18:47:51 +0200

    /

--- get_zara.py
+++ get_zara.py
@@ -1,6 +1,12 @@
 import helper_functions as helper
 from proxy import client
 
+user_agent = [
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+]
+
 headers ={
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate, br',
@@ -12,7 +18,7 @@ headers ={
     'Sec-Fetch-Site':'none',
     'Sec-Fetch-User':'?1',
     'Upgrade-Insecure-Requests':'1',
-    'User-Agent':	'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'
+    'User-Agent': any(user_agent)
 }
 
 response = helper.get_page(
