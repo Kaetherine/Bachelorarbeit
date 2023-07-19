49dd49d1992fbde9dc341172877e5e07cdc3410b
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-19 21:30:36 +0200

    /

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import get_zara as zara
 from to_s3_bucket import upload_json_to_bucket
+from datetime import datetime
+
+date = datetime.now()
 
 def get_zara_data_and_upload():
     category_ids = zara.get_categories()
@@ -8,24 +11,12 @@ def get_zara_data_and_upload():
     product_details = zara.get_product_details(product_ids[14:18])
     related_products = zara.get_related_products(product_ids[14:18])
 
-
-    '''executes all functions to get the content of zara'''
-    # print(category_ids)
-    # print(category_ids[14:18])
-    # print(products_by_category)
-    # print(len(products_by_category))
-    # print(product_ids)
-    # print(product_details)
-    # print(len(product_details))
-    # print(related_products)
-
-
     # saving data to s3bucket
-    upload_json_to_bucket(category_ids, 'category_ids.json')
-    upload_json_to_bucket(products_by_category, 'products_by_category.json')
-    upload_json_to_bucket(product_ids, 'product_ids.json')
-    upload_json_to_bucket(product_details, 'product_details.json')
-    upload_json_to_bucket(related_products, 'related_products.json')
+    upload_json_to_bucket(category_ids, f'{date}-category_ids.json')
+    upload_json_to_bucket(products_by_category, f'{date}-products_by_category.json')
+    upload_json_to_bucket(product_ids, f'{date}-product_ids.json')
+    upload_json_to_bucket(product_details, f'{date}-product_details.json')
+    upload_json_to_bucket(related_products, f'{date}-related_products.json')
 
 
 if __name__ == "__main__":
---
+++ postgres_connection.py
@@ -0,0 +1,30 @@
+from credentials import master_username, master_password, endpoint, dbname
+
+import psycopg2
+
+def connect_to_db():
+    try:
+        connection = psycopg2.connect(
+            host = endpoint,
+            port = '5432',
+            dbname = dbname,
+            user = master_username,
+            password = master_password
+        )
+
+        # cursor object
+        cur = connection.cursor()
+
+        # sample query
+        cur.execute("""SELECT * FROM vendors""")
+        query_results = cur.fetchall()
+        print(query_results)
+
+        # close cursor and connection
+        cur.close()
+        connection.close()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+connect_to_db()
