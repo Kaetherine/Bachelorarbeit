5b6b0fcbc822b580f565f602e913ed37f91319ad
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-19 19:19:31 +0200

    /

--- cron_script.sh
+++ cron_script.sh
@@ -9,7 +9,7 @@ SCRIPT="/home/katherine/Development/Bachelorarbeit/main.py"
 LOGFILE="/home/katherine/Development/Bachelorarbeit/logfile.log"
 
 # send notificationmal to email receipient
-echo -e "Subject: Cronjob for Bachelorarbeit/main.py started #1\n\nThe cronjob started the script now." | sendmail $EMAIL_RECEIPIENT
+echo -e "Subject: Cronjob for Bachelorarbeit/main.py started\n\nThe cronjob started the script now." | sendmail $EMAIL_RECEIPIENT
 
 # execution of python script
 python3 $SCRIPT 2>> $LOGFILE
@@ -20,8 +20,8 @@ LOG_CONTENT=$(tail -n 50 "$LOGFILE")
 # check if script failed or ran successfully
 if [ $? -eq 0 ]; then
     # if successful
-    echo -e "Subject: Bachelorarbeit/main.py successful! #1\n\nThe script Bachelorarbeit/main.py was executed successfully\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
+    echo -e "Subject: Bachelorarbeit/main.py successful!\n\nThe script Bachelorarbeit/main.py was executed successfully\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
 else
     # if failed
-    echo -e "Subject: Bachelorarbeit/main.py failed! #1\n\nExecution of the script Bachelorarbeit/main.py FAILED!\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
+    echo -e "Subject: Bachelorarbeit/main.py failed!\n\nExecution of the script Bachelorarbeit/main.py FAILED!\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
 fi
