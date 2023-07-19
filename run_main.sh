e96124c78ce5c6f519e01419ac782dba5009f2d6
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-20 01:03:09 +0200

    /

--- run_main.sh
+++ run_main.sh
@@ -27,11 +27,7 @@ DURATION=$((END_TIME - START_TIME))
 LOG_CONTENT=$(tail -n 50 "$LOGFILE")
 
 # check if script failed or ran successfully
-if [ $? -eq 0 ]; then
-    # if successful send email and run transform_zara.py
-    echo -e "Subject: Execution completed for Bachelorarbeit/main.py\n\nThe script Bachelorarbeit/main.py was executed. See Logfile to verify it was successfully. It took $DURATION seconds.\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
 
-else
-    # if failed
-    echo -e "Subject: Bachelorarbeit/main.py failed!\n\nExecution of the script Bachelorarbeit/main.py FAILED! It took $DURATION seconds.\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
-fi
+# if successful send email and run transform_zara.py
+echo -e "Subject: Execution completed for Bachelorarbeit/main.py\n\nThe script Bachelorarbeit/main.py was executed. See Logfile to verify it was successfully. It took $DURATION seconds.\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
+
