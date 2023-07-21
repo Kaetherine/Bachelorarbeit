7c3bc2eea0a4f2ef6f172cefbf8ef6798fcc63c2
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-21 19:00:21 +0200

    /

--- etl_zara.py
+++ etl_zara.py
@@ -1,7 +1,19 @@
 from postgres_connection import connect_to_db, disconnect_from_db
+from to_s3_bucket import s3_client
+
+# List all objects in your bucket
+response = s3_client.list_objects(Bucket='your_bucket_name')
+print(response)
+
+# for obj in response['Contents']:
+#     print(obj['Key'])
+
+# # Delete a specific file
+# s3_client.delete_object(Bucket='your_bucket_name', Key='your_file_key')
 
 
 def product_details(json):
+    pass
     '''doctstring here'''
         # filter content
     # product_details = {}
