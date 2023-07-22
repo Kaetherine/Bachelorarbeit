cae7e3c22813e57f0dff977567177f560b6a3e67
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-22 15:30:44 +0200

    /

--- s3_bucket.py
+++ s3_bucket.py
@@ -25,24 +25,30 @@ def upload_json_to_bucket(data, filename, bucket=aws_bucket, ):
         Body=data_byte_stream
     )
 
-def get_bucket(bucket_name):
+def get_bucket(bucket_name=aws_bucket):
+    '''Retrieves a list of all objects in the specified AWS S3 bucket.'''
     response = s3_client.list_objects(Bucket=bucket_name)
     return response
 
-def get_bucket_content(bucket_name):
+def get_bucket_content(bucket_name=aws_bucket):
+    '''Retrieves a list of the keys (filenames) of all 
+    objects in the specified AWS S3 bucket.'''
     response = get_bucket(bucket_name)
     filenames = []
     for obj in response['Contents']:
         filenames.append(obj['Key'])
     return filenames
 
-def delete_object(bucket_name, filename):
+def delete_object(filename, bucket_name=aws_bucket):
+    '''Deletes an object, specified by its filename, from an AWS S3 bucket.'''
     s3_client.delete_object(
         Bucket=bucket_name,
         Key=filename
         )
     
-def get_bucket_file(bucket_name, filename):
+def get_bucket_file(filename, bucket_name=aws_bucket):
+    '''Retrieves a file, specified by its filename, from an AWS S3
+    bucket and returns its content.'''
     file = s3_client.get_object(
         Bucket=bucket_name,
         Key=filename
