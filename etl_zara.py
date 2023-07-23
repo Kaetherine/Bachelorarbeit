1155136743409748acfe7ae3ac2f6e6183f002ec
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-23 12:46:46 +0200

    /

--- etl_zara.py
+++ etl_zara.py
@@ -28,8 +28,8 @@ def products_to_df(list_of_products):
     return df_products
 
 def extract_product_details(product_details, product_id):
-    '''This function extracts care, certified materials, materials, 
-    and origin details for a given product from the product details dataset.'''
+    '''This function extracts care, certified materials, materials, and 
+    origin details for a given product from the product details dataset.'''
     care, certified_materials, materials, origin = None, None, None, None
     for detail in product_details[product_id]:
         section_type = detail['sectionType']
@@ -52,15 +52,16 @@ def extract_certified_materials():
 def extract_materials(materials):
     for i, item in enumerate(materials['components']):
         if 'text' in item and 'value' in item['text']:
-            if 'typography' in item['text'] and item['text']['typography'] in ['heading-s', 'heading-xs']:
-                print(f"Index: {i}, Attributname: {item['text']['value']}")
+            if ('typography' in item['text'] and 
+                item['text']['typography'] in ['heading-s', 'heading-xs']):
+                attribute_name = item['text']['value']
             else:
-                print(f"Index: {i}, Attributwert: {item['text']['value']}")
+                attribute_value = item['text']['value']
 
 def extract_origin():
     pass
 
-
+date = '2023-07-22'
 def organise_product_details():
     product_details = get_bucket_file(f'{date}-product_details.json')
     product = []
@@ -71,21 +72,24 @@ def organise_product_details():
 
     for product_id in product_details:
         extracted_details = extract_product_details(product_details, product_id)
+        
 
-        extracted_care = extract_care(extracted_details[1])
-        care.append(extracted_care)
+        # extracted_care = extract_care(extracted_details[1])
+        # care.append(extracted_care)
 
-        extracted_certified_materials = extract_certified_materials(extracted_details[2])
-        certified_materials.append(extracted_certified_materials)
+        # extracted_certified_materials = extract_certified_materials(extracted_details[2])
+        # certified_materials.append(extracted_certified_materials)
 
         extracted_materials = extract_materials(extracted_details[3])
-        materials.append(extracted_materials)
+        print(extracted_materials)
+        # materials.append(extracted_materials)
 
-        extracted_origin = extract_origin(extracted_details[4])
-        origin.append(extracted_origin)
+        # extracted_origin = extract_origin(extracted_details[4])
+        # origin.append(extracted_origin)
 
-    return [product_id, care, certified_materials, materials, origin]
+    # return materials
 
-
+organise_product_details()
+# print(organised_product_details)
 
 
