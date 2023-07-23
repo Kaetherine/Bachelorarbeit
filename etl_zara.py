99cd864073fd4a7e776f8b4ee40713719f72ecf9
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-23 16:06:38 +0200

    /

--- etl_zara.py
+++ etl_zara.py
@@ -63,16 +63,29 @@ def extract_materials(materials, product_id):
             elif attribute_name is not None:
                 attribute_value = item['text']['value']
                 if attribute_value not in ['COMPOSITION', 'Which contains at least:']:
-                    attribute_value = attribute_value.split('%')
-                    percentage = f'{attribute_value[0]}%'
-                    material = attribute_value[1]
-                    # add a new row for each material
-                    material_dict.append({
-                        'product_id': product_id,
-                        'material_part': attribute_name,
-                        'percentage': percentage,
-                        'material': material
-                        })
+                    if '<br>' in attribute_value:
+                        attribute_value = attribute_value.split('<br>')
+                        for item in attribute_value:
+                            m = item.split('%')
+                            percentage = f'{m[0]}%'
+                            material = m[1]
+                            material_dict.append({
+                                'product_id': product_id,
+                                'material_part': attribute_name,
+                                'percentage': percentage,
+                                'material': material
+                                })
+                    else:
+                        attribute_value = attribute_value.split('%')
+                        percentage = f'{attribute_value[0]}%'
+                        material = attribute_value[1]
+                        # add a new row for each material
+                        material_dict.append({
+                            'product_id': product_id,
+                            'material_part': attribute_name,
+                            'percentage': percentage,
+                            'material': material
+                            })
 
     return material_dict
 
