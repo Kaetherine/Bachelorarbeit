c030d0af40cf595de5d0c18c5b19fb99bae8f6dc
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-23 17:14:57 +0200

    /

--- etl_zara.py
+++ etl_zara.py
@@ -5,10 +5,8 @@ import pandas as pd
 from s3_bucket import *
 from get_zara import extract_products
 
-
-date = datetime.now().strftime('%Y-%m-%d')
-
-
+# date = datetime.now().strftime('%Y-%m-%d')
+date = '2023-07-22'
 
 def normalize_related_products(json):
     result = []
@@ -20,10 +18,8 @@ def normalize_related_products(json):
 def normalize_products_by_categories(json):
     pass
 
-def normalize_products(json):
-    pass
-
 def products_to_df(list_of_products):
+    '''docstring here'''
     df_products = pd.DataFrame(list_of_products)
     return df_products
 
@@ -50,19 +46,21 @@ def extract_certified_materials():
     pass
 
 def extract_materials(materials, product_id):
+    '''docstring here'''
     material_list = []
     attribute_name = None
-    for i, item in enumerate(materials['components']):
+    ignore = ['COMPOSITION', 'Which contains at least:']
+    for item in materials['components']:
         if 'text' in item and 'value' in item['text']:
             if ('typography' in item['text'] and 
                 item['text']['typography'] in ['heading-s', 'heading-xs']):
                 attribute_name = item['text']['value']
-                if attribute_name in ['COMPOSITION', 'Which contains at least:']:
+                if attribute_name in ignore:
                     attribute_name = None
                     continue
             elif attribute_name is not None:
                 attribute_value = item['text']['value']
-                if attribute_value not in ['COMPOSITION', 'Which contains at least:']:
+                if attribute_value not in ignore:
                     if '<br>' in attribute_value:
                         attribute_value = attribute_value.split('<br>')
                         for item in attribute_value:
@@ -90,6 +88,7 @@ def extract_materials(materials, product_id):
     return material_list
 
 def extract_origin(origin, product_id):
+    '''docstring here'''
     origin_list = []
     country_of_origin = origin['components'][-1]['text']['value'].split('Made in')[1]
     origin_list.append({
@@ -104,22 +103,14 @@ def detail_to_df(detail):
     df_detail = pd.DataFrame(detail)
     return df_detail
 
-date = '2023-07-22'
 def organise_product_details():
+    '''docstring here'''
     product_details = get_bucket_file(f'{date}-product_details.json')
-    # care = []
-    # certified_materials = []
     materials = []
     origin = []
 
     for product_id in product_details:
         extracted_details = extract_product_details(product_details, product_id)
-        
-        # extracted_care = extract_care(extracted_details[1])
-        # care.append(extracted_care)
-
-        # extracted_certified_materials = extract_certified_materials(extracted_details[2])
-        # certified_materials.append(extracted_certified_materials)
 
         extracted_materials = extract_materials(extracted_details[3], product_id)
         materials.append(extracted_materials)
