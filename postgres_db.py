1add54a656a17ce5ffe475f3e4cfd8267ed24f50
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-26 21:46:55 +0200

    /

--- postgres_db.py
+++ postgres_db.py
@@ -1,51 +1,136 @@
 from sqlalchemy import create_engine, text
 import psycopg2
 from credentials import master_username, master_password, endpoint, dbname
+import pandas as pd
 
-def connect_to_db():
-    engine = create_engine(
-        f'''postgresql+psycopg2://
-        {master_username}:{master_password}
-        @{endpoint}:5432/{dbname}'''
-        )
-    return engine
+import csv
+import os
+import subprocess
 
-def test_connection(engine):
-    try:
-        with engine.connect() as connection:
-            result = connection.execute(text("SELECT 1"))
-            print(f"Connection successful: {result.scalar() == 1}")
-    except Exception as e:
-        print(f"Connection failed: {e}")
+# Erstellen Sie zuerst eine CSV-Datei aus Ihren Daten
+fake_data = [
+    ('Germany', 897.4),
+    ('France', 458.7),
+    ('United Kingdom', 423.2),
+    ('Italy', 402.3),
+    ('Spain', 333.1),
+    ('Poland', 320.9),
+    ('Netherlands', 196.3),
+    ('Belgium', 119.3),
+    ('Sweden', 55.1),
+    ('Austria', 78.7),
+    ('Greece', 71.1),
+    ('Portugal', 59.3),
+    ('Denmark', 42.8),
+    ('Finland', 53.6),
+    ('Ireland', 39.1),
+]
 
-db_connection = connect_to_db()
-test_connection(db_connection)
-# DataFrame in die Datenbank schreiben
-# df.to_sql('table_name', engine, if_exists='append', chunksize=500)
+csv_file = 'fake_data.csv'
+with open(csv_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(fake_data)
+
+table_name = 'co2country'
+copy_command = fr'''\COPY {table_name} FROM '/home/katherine/Development/Bachelorarbeit/{csv_file}' CSV'''
+
+os.environ['PGPASSWORD'] = master_password
+command = ['psql', '-h', endpoint, '-p', '5432', '-U', master_username, '-d', dbname, '-c', copy_command]
+
+process = subprocess.Popen(command, stdout=subprocess.PIPE)
+process.wait()
+print(process.returncode)
+
+output = process.stdout.read()
+print(output)
+
+
+# fake_data = [
+#     ('Germany', 2023, 897.4),
+#     ('France', 2023, 458.7),
+#     ('United Kingdom', 2023, 423.2),
+#     ('Italy', 2023, 402.3),
+#     ('Spain', 2023, 333.1),
+#     ('Poland', 2023, 320.9),
+#     ('Netherlands', 2023, 196.3),
+#     ('Belgium', 2023, 119.3),
+#     ('Sweden', 2023, 55.1),
+#     ('Austria', 2023, 78.7),
+#     ('Greece', 2023, 71.1),
+#     ('Portugal', 2023, 59.3),
+#     ('Denmark', 2023, 42.8),
+#     ('Finland', 2023, 53.6),
+#     ('Ireland', 2023, 39.1),
+# ]
+
+# sql_command = '''INSERT INTO test (pk, colum1) VALUES ('es funktiniert', 'doch');'''
+
+# os.environ['PGPASSWORD'] = master_password
+# command = ['psql', '-h', endpoint, '-p', '5432', '-U', master_username, '-d', dbname, '-c', sql_command]
+
+# process = subprocess.Popen(command, stdout=subprocess.PIPE)
+# process.wait()
+# print(process.returncode)
+
+# output = process.stdout.read()
+# print(output)
+
+
+
+# import subprocess
+
+# sql_command = '''INSERT INTO test (pk, colum1) VALUES ('hallo', 'tschuess');'''
+
+# command = f'''
+#     export PGPASSWORD='{master_password}'; 
+#     psql -h {endpoint} -p 5432 -U {master_username} -d {dbname} -c "{sql_command}" '''
+
+# process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+# process.wait()
+# print(process.returncode)
+
+# output = process.stdout.read()
+# print(output)
 
 # def connect_to_db():
-#     '''establishes the db connection and return
-#     cursor and connection object'''
-#     connection = psycopg2.connect(
-#         host = endpoint,
-#         port = '5432',
-#         dbname = dbname,
-#         user = master_username,
-#         password = master_password
+#     engine = create_engine(
+#     f'''postgresql://
+#     {master_username}:{master_password}
+#     @{endpoint}:5432/{dbname}'''
 #     )
+#     return engine
 
-#         # cursor object
-#     cur = connection.cursor()
-#     return cur, connection
+# def test_connection(engine):
+#     try:
+#         with engine.connect() as connection:
+#             result = connection.execute('select * from test')
+#             print(f"Connection successful: {result.scalar() == 1}")
+#     except Exception as e:
+#         print(f"Connection failed: {e}")
 
-# def disconnect_from_db(cur, connection):
-#     '''close cursor and connection'''
-#     cur.close()
-#     connection.close()
+# data = {
+#     'name': ['Alice', 'Bob', 'Charlie'],
+#     'age': [25, 45, 35],
+#     'city': ['New York', 'Los Angeles', 'Chicago']
+# }
 
-        
-# # # sample query
-# # cur.execute("""SELECT * FROM vendors""")
-# # query_results = cur.fetchall()
-# # print(query_results)
+# df = pd.DataFrame(data)
+# db_connection = connect_to_db()
+
+# df.to_sql('my_table', db_connection, if_exists='replace')
+# import subprocess
+
+# sql_command = '''INSERT INTO test (pk, colum1) VALUES ('hallo', 'tschuess');'''
+
+# command = f'''
+#     export PGPASSWORD='{master_password}'; 
+#     psql -h {endpoint} -p 5432 -U {master_username} -d {dbname} -c "{sql_command}" '''
+
+# process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+# process.wait()
+# print(process.returncode)
+
+# output = process.stdout.read()
+# print(output)
+
 
