022be4d96ea38605c7c05969a72b8ea3b206d3a9
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-27 22:10:44 +0200

    renamed pantone setup file to pantone_setup.py

---
+++ pantone_setup.py
@@ -0,0 +1,53 @@
+from postgres_db import copy_csv_to_db
+from s3_bucket import get_bucket_file
+from helpers import csv_path
+
+def normalize_pantone(filename):
+    '''Takes a pantone catalog as json file and normalizes it
+    by seperating it into two tables 'color_configs' and 'color_names'.
+    Returns color_configs and color_names of given pantone color catalog.
+    '''
+    color_data = get_bucket_file(filename)
+    color_configs = []
+    color_names = []
+    for color in color_data['data']['getBook']['colors']:
+        color_configs.append((
+            f"#{color['hex']}", color['rgb']['r'],
+            color['rgb']['g'], color['rgb']['b'],
+            color['lab']['l'], color['lab']['a'],
+            color['lab']['b'], color['cmyk'],
+            color['code']
+            ))
+        
+        color_names.append((color['code'],color['name']))
+
+    return color_configs, color_names
+
+def organize_colors():
+    '''Normalizes pantone color data, merges the color_configs and 
+    color_names of different pantone catalouges. Returns merged
+    color_configs and color_names.'''
+    color_configs = []
+    color_names = []
+
+    tcx_color_configs, tcx_color_names = normalize_pantone('pantone_tcx.json')
+    tn_color_configs, tn_color_names = normalize_pantone('pantone_tn.json')
+    tpg_color_configs, tpg_color_names = normalize_pantone('pantone_tpg.json')
+    tsx_color_configs, tsx_color_names = normalize_pantone('pantone_tsx.json')
+
+    color_configs.extend(tcx_color_configs)
+    color_configs.extend(tn_color_configs)
+    color_configs.extend(tpg_color_configs)
+    color_configs.extend(tsx_color_configs)
+
+    color_names.extend(tcx_color_names)
+    color_names.extend(tn_color_names)
+    color_names.extend(tpg_color_names)
+    color_names.extend(tsx_color_names)
+
+    return color_configs, color_names
+
+# onetime execution
+color_configs, color_names = organize_colors()
+copy_csv_to_db(color_configs, f'{csv_path}hex_colors.csv', pk_columns='hex_color')
+copy_csv_to_db(color_names, f'{csv_path}color_names.csv', pk_columns='color_code')
