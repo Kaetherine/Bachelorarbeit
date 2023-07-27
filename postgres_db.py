3e7975e7149319cc6922cfd21fd20bf22809f0d0
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-27 13:46:04 +0200

    /

--- postgres_db.py
+++ postgres_db.py
@@ -4,14 +4,15 @@ import os
 import subprocess
 
 def copy_csv_to_db(data, csv_file_name, table_name, primary_key_columns):
+    '''docstring here'''
     csv_file_name = create_csv_file(data, csv_file_name)
 
     commands = [
-        f"DROP TABLE IF EXISTS tmp_{table_name}",
-        f"CREATE TABLE tmp_{table_name} AS SELECT * FROM {table_name} LIMIT 0",
-        f"\COPY tmp_{table_name} FROM '{csv_file_name}' CSV",
-        f"INSERT INTO {table_name} SELECT * FROM tmp_{table_name} ON CONFLICT ({primary_key_columns}) DO NOTHING",
-        f"DROP TABLE tmp_{table_name}",
+        f'DROP TABLE IF EXISTS tmp_{table_name}',
+        f'CREATE TABLE tmp_{table_name} AS SELECT * FROM {table_name} LIMIT 0',
+        f'\COPY tmp_{table_name} FROM "{csv_file_name}" CSV',
+        f'INSERT INTO {table_name} SELECT * FROM tmp_{table_name} ON CONFLICT ({primary_key_columns}) DO NOTHING',
+        f'DROP TABLE tmp_{table_name}',
     ]
 
     os.environ['PGPASSWORD'] = master_password
