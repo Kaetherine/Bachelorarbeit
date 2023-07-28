d9be55e8a497da20f06aa195984a35c54b621458
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-28 20:02:50 +0000

    /

--- run_main.sh
+++ run_main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-EMAIL_RECEIPIENT="kuhle.katherine@gmail.com"
+EMAIL_RECEIPIENT='kuhle.katherine@gmail.com'
 
 # main file to be executed
-SCRIPT="/home/katherine/Development/Bachelorarbeit/main.py"
+SCRIPT='/home/ubuntu/Bachelorarbeit/main.py'
 
 # path to logfile
-LOGFILE="/home/katherine/Development/Bachelorarbeit/logfile.log"
+LOGFILE='/home/ubuntu/Bachelorarbeit/logfile.log'
 
 # send notificationmal to email receipient
-echo -e "Subject: Cronjob for Bachelorarbeit/main.py started\n\nThe cronjob started the script now." | sendmail $EMAIL_RECEIPIENT
+echo -e '''Subject: Cronjob for Bachelorarbeit/main.py started\n\n
+    The cronjob started the script now.''' | sendmail $EMAIL_RECEIPIENT
 
 # get start time
 START_TIME=$(date +%s)
@@ -24,11 +25,25 @@ END_TIME=$(date +%s)
 DURATION=$((END_TIME - START_TIME))
 
 # read last 50 lines of logfile
-LOG_CONTENT=$(tail -n 50 "$LOGFILE")
+# LOG_CONTENT=$(tail -n 50 "$LOGFILE")
+
+# whole logfile
+LOG_CONTENT=$LOGFILE
 
 # check if script failed or ran successfully
 
 # if successful send email and run transform_zara.py
-echo -e "Subject: Execution completed for Bachelorarbeit/main.py\n\nThe script Bachelorarbeit/main.py was executed. See Logfile to verify it was successfully. It took $DURATION seconds.\n\nLogfile content:\n$LOG_CONTENT" | sendmail $EMAIL_RECEIPIENT
+echo -e '''Subject: Execution completed for Bachelorarbeit/main.py\n\nThe script 
+    Bachelorarbeit/main.py was executed. See Logfile to verify it was successfully. 
+    It took $DURATION seconds.\n\nLogfile content:\n$LOG_CONTENT''' | sendmail $EMAIL_RECEIPIENT
 
-# rm filename
+# rm logfile.log
+# rm categories.csv
+# rm categories_by_target_groups.csv
+# rm materials.csv
+# rm origins.csv
+# rm product_availability.csv
+# rm products.csv
+# rm products_by_categories.csv
+# rm related_products.csv
+# rm target_groups.csv
