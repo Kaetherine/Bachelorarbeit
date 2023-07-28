a62a44a68aa8783a6b11cc7c861648964e04f9ad
Kaetherine <kuhle.katherine@googlemail.com>
2023-07-28 22:11:57 +0000

    /

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_zara_data_and_upload_to_s3_bucket():
     upload_json_to_bucket(category_ids, f'{date}-category_ids.json')
 
     products_by_category = {}
-    for category_id in category_ids['category_ids'][10:12]:
+    for category_id in category_ids['category_ids']:
         try:
             products_by_category[category_id] = get_zara.get_product_list(category_id)
         except Exception as e:
@@ -38,7 +38,7 @@ def get_zara_data_and_upload_to_s3_bucket():
     upload_json_to_bucket(product_ids, f'{date}-product_ids.json')
 
     product_details = {}
-    for product_id in product_ids['product_ids'][:2]:
+    for product_id in product_ids['product_ids']:
         try:
             product_details[product_id] = get_zara.get_product_details(product_id)
         except Exception as e:
@@ -47,7 +47,7 @@ def get_zara_data_and_upload_to_s3_bucket():
         upload_json_to_bucket(product_details, f'{date}-product_details.json')
 
     related_products = {}
-    for product_id in product_ids['product_ids'][:2]:
+    for product_id in product_ids['product_ids']:
         try:
             related_products[product_id] = get_zara.get_related_products(product_id)
         except Exception as e:
